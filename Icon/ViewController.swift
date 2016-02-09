b35c18fb3d49a2b41ba3c7830d6b941e7af7c507
南部匡寛 <m.nambu1121@gmail.com>
2016-02-09 17:45:49 +0900

    Initial Commit

---
+++ Icon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Icon
+//
+//  Created by 南部　匡寛 on 2016/02/09.
+//  Copyright © 2016年 南部匡寛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
